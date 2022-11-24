0de32488ffda4da23c036928f6fd0996866a3154
shrish26 <82869803+shrish26@users.noreply.github.com>
2022-11-24 10:21:56 +0530

    'second'

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -27,7 +27,7 @@ const Contact = () => {
               className="p-2 bg-transparent border-2 rounded-md text-white focus:outline-none"
             />
             <input
-              type="text"
+              type="email"
               name="email"
               placeholder="Enter your email"
               className="my-4 p-2 bg-transparent border-2 rounded-md text-white focus:outline-none"
