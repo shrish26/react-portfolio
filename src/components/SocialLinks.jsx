146e268244ef086c87fc2996f1b3bc019915f1cd
shrish26 <82869803+shrish26@users.noreply.github.com>
2023-04-19 00:24:30 +0530

    Resume

--- src/components/SocialLinks.jsx
+++ src/components/SocialLinks.jsx
@@ -40,7 +40,7 @@ function SocialLinks() {
         Resume<BsFillPersonLinesFill size={30}/>
         </>
       ),
-      href:'./resume.pdf',
+      href:'/RESUME.pdf',
       style:'rounded-br-md',
       download:true,
     }
